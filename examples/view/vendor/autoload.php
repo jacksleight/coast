80e701f2b37bff18084ae7f040d24aed09a359eb
Jack Sleight <hi@jacksleight.com>
2014-01-30 09:56:38 +0000

    Update placeholder autoloaders

--- examples/view/vendor/autoload.php
+++ examples/view/vendor/autoload.php
@@ -1,7 +1,5 @@
 <?php
-// This file is a hack to allow the examples to work from the main library.
-// Don't do this, use Composer's autoloader instead, as per the example in /README.md.
-
+// Placeholder autoloader, allows the examples to work from the main library
 require '../../lib/Coast.php';
 set_include_path(get_include_path() . PATH_SEPARATOR . '../../lib');
 spl_autoload_register();
