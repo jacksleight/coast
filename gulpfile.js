33abd2adf39bc4271f8c6d5bc5c5bc16716afdd5
Jack Sleight <hi@jacksleight.com>
2014-02-26 11:47:05 +0000

    Added gulp for live phpunit testing.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp')
+	phpunit = require('gulp-phpunit');
+
+gulp.task('phpunit', function() {
+    gulp.src('tests/**/*.php')
+    	.pipe(phpunit('', {clear: true}));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('tests/**/*.php', ['phpunit']);
+});
