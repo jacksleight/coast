2619162826b6d43b8d762e83c8fa35ae13f6c05c
Jack Sleight <hi@jacksleight.com>
2014-03-31 22:43:25 +0100

    Copyright on model class.

--- lib/Coast/Model.php
+++ lib/Coast/Model.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Copyright 2014 Jack Sleight <http://jacksleight.com/>
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENCE. 
+ */
+
 namespace Coast;
 
 class Model
