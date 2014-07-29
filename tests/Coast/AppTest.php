a6756f897a8a562084f01983c096ba1b35a1e18d
Jack Sleight <hi@jacksleight.com>
2014-07-29 21:50:53 +0100

    Fix tests.

--- tests/Coast/AppTest.php
+++ tests/Coast/AppTest.php
@@ -4,19 +4,20 @@ namespace Coast;
 use Coast\App, 
     Coast\App\Request,
     Coast\App\Response,
-    Coast\App\Url;
+    Coast\App\UrlResolver,
+    Coast\Url;
 
 class AppTest extends \PHPUnit_Framework_TestCase
 {
     public function testEnv()
     {
-        $app = new App(['DEBUG' => true]);
+        $app = new App(__DIR__, ['DEBUG' => true]);
         $this->assertTrue($app->env('DEBUG'));
     }
 
     public function testMode()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $this->assertEquals(App::MODE_CLI, $app->mode());
         $this->assertTrue($app->isCli());
         $this->assertFalse($app->isHttp());
@@ -24,59 +25,59 @@ class AppTest extends \PHPUnit_Framework_TestCase
 
     public function testExecute()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $app->add(function(Request $req, Response $res) {
             return $res->text('OK');
         });
         $app->add('test', function(Request $req, Response $res) {
             return $res->text('OK');
         });
-        $res = $app->execute(new Request());
+        $app->execute($req = new Request(), $res = new Response($req));
         $this->assertEquals('OK', $res->body());
     }
 
     public function testExecuteException()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $this->setExpectedException('Coast\App\Exception');
         $app->execute(new Request());
     }
 
     public function testInvalid()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $this->setExpectedException('Coast\App\Exception');
         $app->add(true);
     }
 
     public function testNotFoundHandler()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $app->notFoundHandler(function(Request $req, Response $res) {
             return $res->text('NOT FOUND');
         });
-        $res = $app->execute(new Request());
+        $app->execute($req = new Request(), $res = new Response($req));
         $this->assertEquals('NOT FOUND', $res->body());
     }
 
     public function testErrorHandler()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $app->errorHandler(function(Request $req, Response $res) {
             return $res->text('ERROR');
         });
-        $res = $app->execute(new Request());
+        $app->execute($req = new Request(), $res = new Response($req));
         $this->assertEquals('ERROR', $res->body());
     }
 
     public function testParam()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $app->set('test', 'OK');
         $this->assertTrue($app->has('test'));
         $this->assertEquals('OK', $app->get('test'));
 
-        $app = new App();
+        $app = new App(__DIR__);
         $app->test = 'OK';
         $this->assertTrue(isset($app->test));
         $this->assertEquals('OK', $app->test);
@@ -84,7 +85,7 @@ class AppTest extends \PHPUnit_Framework_TestCase
 
     public function testParams()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $params = [
             'test1' => 'OK',
             'test2' => 'OK',
@@ -96,14 +97,14 @@ class AppTest extends \PHPUnit_Framework_TestCase
 
     public function testAccess()
     {
-        $app = new App();
-        $app->set('url', new Url());
+        $app = new App(__DIR__);
+        $app->set('url', new UrlResolver(new Url('/')));
         $this->assertEquals('/test', $app->url('test')->toString());
     }
 
     public function testAccessException()
     {
-        $app = new App();
+        $app = new App(__DIR__);
         $app->set('invalid', true);
         $this->setExpectedException('Coast\App\Exception');
         $app->invalid();
