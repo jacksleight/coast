8f8590895d22d4f9580b6d93eeb76c66b4c164bb
Jack Sleight <jacksleight@gmail.com>
2015-01-04 13:57:53 +0000

    Add string validator.

---
+++ lib/Validator/Rule/String.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright 2015 Jack Sleight <http://jacksleight.com/>
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENCE. 
+ */
+
+namespace Coast\Validator\Rule;
+
+use Coast\Validator\Rule;
+
+class String extends Rule
+{
+	protected function _validate($value)
+	{
+		if ($value !== null && !is_scalar($value) && !is_callable([$value, '__toString'])) {
+		 	$this->error();
+		}
+	}
+}
