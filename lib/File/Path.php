3d9019f752c3585e60f3c549a950e0208fc1ab12
Jack Sleight <jacksleight@gmail.com>
2015-07-02 22:38:32 +0100

    File and Path API tweaks

--- lib/File/Path.php
+++ lib/File/Path.php
@@ -8,79 +8,7 @@ namespace Coast\File;
 
 abstract class Path extends \Coast\Path
 {
-    /**
-     * @todo isReal?
-     */
-    public function exists()
-    {
-        return file_exists($this->_name);
-    }
-
-    public function isDir()
-    {
-        return is_dir($this->_name);
-    }
-
-    public function isFile()
-    {
-        return is_file($this->_name);
-    }
-
-    public function isReadable()
-    {
-        return is_readable($this->_name);
-    }
-
-    public function isWritable()
-    {
-        return is_writable($this->_name);
-    }
-
-    public function permissions()
-    {
-        return substr(sprintf('%o', fileperms($this->_name)), -4);
-    }
-
-    public function move(\Coast\Dir $dir, $baseName = null)
-    {
-        $name = "{$dir}/" . (isset($baseName)
-            ? $this->_parseBaseName($baseName)
-            : $this->baseName());
-        rename($this->_name, $name);
-        $this->_name = $name;
-        return $this;
-    }
-
-    public function rename($baseName, \Coast\Dir $dir = null)
-    {
-        $name = (isset($dir)
-            ? $dir
-            : $this->dir()) . "/{$this->_parseBaseName($baseName)}";
-        rename($this->_name, $name);
-        $this->_name = $name;
-        return $this;
-    }
-
     abstract public function copy(\Coast\Dir $dir, $baseName = null);
     
     abstract public function remove();
-
-    public function _parseBaseName($baseName)
-    {
-        if (is_array($baseName)) {
-            $baseName = array_intersect_key($baseName, [
-                'baseName' => null,
-                'fileName' => null,
-                'extName'  => null,
-                'prefix'   => null,
-                'suffix'   => null,
-            ]);
-            $path = new \Coast\Path($this->baseName());
-            foreach ($baseName as $method => $value) {
-                $path->$method($value);
-            }
-            $baseName = $path->baseName();
-        }
-        return $baseName;
-    }
 }
--- lib/Path.php
+++ lib/Path.php
@@ -6,6 +6,9 @@
 
 namespace Coast;
 
+use Coast\File;
+use Coast\Dir;
+
 /**
  * Path object.
  */
@@ -242,5 +245,93 @@ class Path
     public function isRelative()
     {
         return !$this->isAbsolute();
+    }
+
+    public function path($path)
+    {
+        $path = ltrim($path, '/');
+        return new Path("{$this->_name}/{$path}");
+    }
+
+    public function toDir()
+    {
+        return new Dir($this->_name);
+    }
+
+    public function toFile()
+    {
+        return new File($this->_name);
+    }
+
+    /**
+     * @todo isReal?
+     */
+    public function exists()
+    {
+        return file_exists($this->_name);
+    }
+
+    public function isDir()
+    {
+        return is_dir($this->_name);
+    }
+
+    public function isFile()
+    {
+        return is_file($this->_name);
+    }
+
+    public function isReadable()
+    {
+        return is_readable($this->_name);
+    }
+
+    public function isWritable()
+    {
+        return is_writable($this->_name);
+    }
+
+    public function permissions()
+    {
+        return substr(sprintf('%o', fileperms($this->_name)), -4);
+    }
+
+    public function move(\Coast\Dir $dir, $baseName = null)
+    {
+        $name = "{$dir}/" . (isset($baseName)
+            ? $this->_parseBaseName($baseName)
+            : $this->baseName());
+        rename($this->_name, $name);
+        $this->_name = $name;
+        return $this;
+    }
+
+    public function rename($baseName, \Coast\Dir $dir = null)
+    {
+        $name = (isset($dir)
+            ? $dir
+            : $this->dir()) . "/{$this->_parseBaseName($baseName)}";
+        rename($this->_name, $name);
+        $this->_name = $name;
+        return $this;
+    }
+
+    protected function _parseBaseName($baseName)
+    {
+        if (is_array($baseName)) {
+            $baseName = array_intersect_key($baseName, [
+                'baseName' => null,
+                'fileName' => null,
+                'extName'  => null,
+                'prefix'   => null,
+                'suffix'   => null,
+            ]);
+            $path = new \Coast\Path($this->baseName());
+            foreach ($baseName as $method => $value) {
+                $path->$method($value);
+            }
+            $baseName = $path->baseName();
+        }
+        return $baseName;
     }
 }
