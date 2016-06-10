ce95363b99370cc0129d71532b255fde52630386
Jack Sleight <jacksleight@gmail.com>
2016-06-10 18:03:11 +0100

    Move Nullify to a transformer

--- lib/Transformer/Rule/Null.php
+++ lib/Transformer/Rule/Null.php
@@ -4,15 +4,18 @@
  * This source file is subject to the MIT license that is bundled with this package in the file LICENCE. 
  */
 
-namespace Coast\Filter\Rule;
+namespace Coast\Transformer\Rule;
 
 use Coast;
-use Coast\Filter\Rule;
+use Coast\Transformer\Rule;
 
-class Nullify extends Rule
+class Null extends Rule
 {
     protected function _filter($value)
     {
+        if (!is_string($value)) {
+            return $value;
+        }
         return strlen($value)
         	? $value
         	: null;
