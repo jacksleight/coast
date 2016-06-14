2032c8ee4ea57852a6c881f3f26cf1281425172c
Jack Sleight <jacksleight@gmail.com>
2016-06-14 19:00:47 +0100

    Tweak metadata class name property

--- lib/Model/Metadata.php
+++ lib/Model/Metadata.php
@@ -14,23 +14,23 @@ use Coast\Transformer;
 
 class Metadata 
 {
-    protected $_name;
+    protected $_className;
 
     protected $_properties = [];
 
-    public function __construct($name)
+    public function __construct($className)
     {
-        $this->_name = $name;
+        $this->_className = $className;
     }
 
-    public function name()
+    public function className()
     {
-        return $this->_name;
+        return $this->_className;
     }
 
     public function property($name, array $value = null)
     {
-        if (!property_exists($this->_name, $name)) {
+        if (!property_exists($this->_className, $name)) {
             return null;
         }
         if (func_num_args() > 1) {
