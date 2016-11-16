c852fb71b16ed91387dbad1c6e383ff82c803510
Jack Sleight <jacksleight@gmail.com>
2016-11-16 19:22:49 +0000

    Refactor image api, add quality and prefix params

--- lib/Image.php
+++ lib/Image.php
@@ -20,6 +20,8 @@ class Image implements \Coast\App\Access, \Coast\App\Executable
 
     protected $_manager;
 
+    protected $_prefix = null;
+
     protected $_driver = 'gd';
 
     protected $_baseDir;
@@ -30,7 +32,9 @@ class Image implements \Coast\App\Access, \Coast\App\Executable
 
     protected $_outputResolver;
 
-    protected $_transforms = [];
+    protected $_quality = 80;
+
+    protected $_actions = [];
 
     public function __construct(array $options = array())
     {
@@ -44,6 +48,15 @@ class Image implements \Coast\App\Access, \Coast\App\Executable
         $this->_manager = new ImageManager([
             'driver' => $this->_driver,
         ]);
+    }
+
+    public function prefix($prefix = null)
+    {
+        if (func_num_args() > 0) {
+            $this->_prefix = $prefix;
+            return $this;
+        }
+        return $this->_prefix;
     }
 
     public function driver($driver = null)
@@ -91,33 +104,42 @@ class Image implements \Coast\App\Access, \Coast\App\Executable
         return $this->_outputResolver;
     }
 
-    public function transform($name, $value = null)
+    public function quality($quality = null)
     {
-        if (isset($value)) {
-            $this->_transforms[$name] = $value->bindTo($this);
+        if (isset($quality)) {
+            $this->_quality = $quality;
             return $this;
         }
-        return isset($this->_transforms[$name])
-            ? $this->_transforms[$name]
+        return $this->_quality;
+    }
+
+    public function action($name, $value = null)
+    {
+        if (isset($value)) {
+            $this->_actions[$name] = $value->bindTo($this);
+            return $this;
+        }
+        return isset($this->_actions[$name])
+            ? $this->_actions[$name]
             : null;
     }
 
-    public function transforms(array $transforms = null)
+    public function actions(array $actions = null)
     {
-        if (isset($transforms)) {
-            foreach ($transforms as $name => $value) {
-                $this->transform($name, $value);
+        if (isset($actions)) {
+            foreach ($actions as $name => $value) {
+                $this->action($name, $value);
             }
             return $this;
         }
-        return $this->_transforms;
+        return $this->_actions;
     }
 
-    public function __invoke($file, $transforms, array $params = array())
+    public function url($file, $actions, array $params = array())
     {
-        if (\Coast\is_array_assoc($transforms)) {
-            $params = $transforms;
-            $transforms = ['default'];
+        if (\Coast\is_array_assoc($actions)) {
+            $params = $actions;
+            $actions = ['default'];
         }
 
         $file = !$file instanceof File
@@ -133,39 +155,39 @@ class Image implements \Coast\App\Access, \Coast\App\Executable
             return $this->_resolver->file($file);
         }
 
-        $transforms = (array) $transforms;
-        foreach ($transforms as $i => $name) {
-            if (!isset($this->_transforms[$name])) {
-                throw new Image\Exception("Transform '{$name}' is not defined");
-            }
-            $transforms[$i] = (string) $name;
+        if (!is_array($actions)) {
+            $actions = array_map('trim', explode(',', $actions));
         }
         foreach ($params as $name => $value) {
             $params[(string) $name] = (string) $value;
         }
         
-        $path   = $file->toRelative($this->_baseDir);
-        $output = $this->_generateOutput($file, $transforms, $params);
+        $output = $this->_generateOutput($file, $actions, $params);
 
         return $output->exists()
             ? (isset($this->_outputResolver)
                 ? $this->_outputResolver->file($output) 
                 : $this->_resolver->file($output))
-            : $this->_resolver->string('image')->queryParams([
-                'file'          => $path->name(),
-                'transforms'    => $transforms,
-                'params'        => $params,
+            : $this->_resolver->string($this->_prefix)->queryParams([
+                'file'    => $file->toRelative($this->_baseDir)->name(),
+                'actions' => $actions,
+                'params'  => $params,
             ]);
     }
 
-    public function execute(Request $req, Response $res)
+    public function process($file, $actions, array $params = array())
     {
-        $parts = explode('/', $req->path());
-        if ($parts[0] != 'image') {
-            return;
+        if (\Coast\is_array_assoc($actions)) {
+            $params = $actions;
+            $actions = ['default'];
         }
 
-        $file = new File("{$this->_baseDir}/{$req->file}");
+        $file = !$file instanceof File
+            ? new File("{$file}")
+            : $file;
+        $file = $file->isRelative()
+            ? new File("{$this->_baseDir}/{$file}")
+            : $file;
         $file = $file->toReal();
         if (!$file->isWithin($this->_baseDir)) {
             throw new Image\Exception("File '{$file}' is not within base directory '{$this->_baseDir}'");
@@ -173,34 +195,56 @@ class Image implements \Coast\App\Access, \Coast\App\Executable
             throw new Image\Exception("File '{$file}' is not readable");
         }
 
-        $transforms = $req->transforms;
-        $params     = isset($req->params) ? $req->params : [];
-        $output     = $this->_generateOutput($file, $transforms, $params);
+        if (!is_array($actions)) {
+            $actions = array_map('trim', explode(',', $actions));
+        }
+
+        $output = $this->_generateOutput($file, $actions, $params);
 
         $image = $this->_manager->make($file->name());
-        foreach ($transforms as $name) {
+        foreach ($actions as $name) {
             $this->run($name, $image, $params);
         }
-        $image->save($output->name(), isset($image->quality) ? $image->quality : 90);
+        $image->save($output->name(), isset($image->quality) ? $image->quality : $this->_quality);
 
-        return $res->redirect(isset($this->_outputResolver)
-            ? $this->_outputResolver->file($output)
-            : $this->_resolver->file($output));
+        return $output;
     }
 
     public function run($name, InterventionImage $image, array $params = array())
     {
-        $this->_transforms[$name]($image, $params);
+        if (!isset($this->_actions[$name])) {
+            throw new Image\Exception("Transform '{$name}' is not defined");
+        }
+        $this->_actions[$name]($image, $params);
     }
 
-    protected function _generateOutput(File $file, $transforms, array $params)
+    public function execute(Request $req, Response $res)
     {
-        sort($transforms);
+        $parts = explode('/', $req->path());
+        if ($parts[0] != $this->_prefix) {
+            return;
+        }
+
+        $output = $this->process($req->file, $req->actions, $req->params);
+
+        return isset($this->_outputResolver)
+            ? $res->redirect($this->_outputResolver->file($output))
+            : $res->redirect($this->_resolver->file($output));
+    }
+
+    public function __invoke($file, $actions, array $params = array())
+    {
+        return $this->url($file, $actions, $params);
+    }
+
+    protected function _generateOutput(File $file, $actions, array $params)
+    {
+        sort($actions);
         ksort($params);
         $id = md5(
             $file->name() .
             $file->modifyTime()->getTimestamp() .
-            serialize($transforms) .
+            serialize($actions) .
             serialize($params)
         );
         return $this->_outputDir
