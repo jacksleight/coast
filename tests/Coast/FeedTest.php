d00f3140afa9543bb43254e8a88c4ac375e6917e
Jack Sleight <jacksleight@gmail.com>
2017-01-13 14:45:45 +0000

    Fix feed test

--- tests/Coast/FeedTest.php
+++ tests/Coast/FeedTest.php
@@ -13,21 +13,22 @@ class FeedTest extends \PHPUnit_Framework_TestCase
         $this->_feed = new Feed();
         $this->_feed->fromArray([
             'id'         => '1000',
-            'title'      => 'Coast',
             'url'        => new \Coast\Url('http://coastphp.com/'),
+            'urlFeed'    => new \Coast\Url('http://coastphp.com/'),
+            'title'      => 'Coast',
             'updateDate' => new \DateTime('2014-01-01'),
             'author'     => ['name' => 'Jack Sleight'],
         ]);
         $this->_feed->items[] = (new Feed\Item())->fromArray([
             'id'         => '1000',
-            'title'      => 'Example Article',
             'url'        => new \Coast\Url('http://coastphp.com/'),
+            'title'      => 'Example Article',
             'updateDate' => new \DateTime('2014-01-01'),
             'summary'    => ['content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, sunt.'],
             'body'       => ['content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, ut sequi labore consequuntur porro illo nisi, vero in molestias non beatae placeat, officia repellat quas eligendi dolor facere, nulla expedita!'],
         ]);
         $this->_output = '<?xml version="1.0" encoding="UTF-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom"><id>1000</id><title><![CDATA[Coast]]></title><link href="http://coastphp.com/"/><updated>2014-01-01T00:00:00+00:00</updated><author><name><![CDATA[Jack Sleight]]></name></author><item><id>1000</id><title><![CDATA[Example Article]]></title><link href="http://coastphp.com/"/><updated>2014-01-01T00:00:00+00:00</updated><summary type="html"><![CDATA[Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, sunt.]]></summary><content type="html"><![CDATA[Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, ut sequi labore consequuntur porro illo nisi, vero in molestias non beatae placeat, officia repellat quas eligendi dolor facere, nulla expedita!]]></content></item></feed>
+<feed xmlns="http://www.w3.org/2005/Atom"><id>1000</id><link href="http://coastphp.com/"/><link rel="self" href="http://coastphp.com/"/><title><![CDATA[Coast]]></title><updated>2014-01-01T00:00:00+00:00</updated><author><name><![CDATA[Jack Sleight]]></name></author><entry><id>1000</id><title><![CDATA[Example Article]]></title><link href="http://coastphp.com/"/><updated>2014-01-01T00:00:00+00:00</updated><summary type="html"><![CDATA[Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, sunt.]]></summary><content type="html"><![CDATA[Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, ut sequi labore consequuntur porro illo nisi, vero in molestias non beatae placeat, officia repellat quas eligendi dolor facere, nulla expedita!]]></content></entry></feed>
 ';
     }
 
