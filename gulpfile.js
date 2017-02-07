8a82627ea0a019161198fe8341d88fd287e89847
Jack Sleight <jacksleight@gmail.com>
2017-02-07 14:49:06 +0000

    Fix phpunit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-var gulp = require('gulp')
-	phpunit = require('gulp-phpunit');
+var gulp    = require('gulp');
+var gutil   = require('gulp-util');
+var phpunit = require('gulp-phpunit');
+var plumber = require('gulp-plumber');
+
+var errorHandler = function(error) {
+    gutil.log(error.toString());
+    this.emit('end');
+};
 
 gulp.task('phpunit', function() {
     gulp.src('tests/**/*.php')
-    	.pipe(phpunit('', {clear: true}));
+        .pipe(plumber({errorHandler: errorHandler}))
+    	.pipe(phpunit('', {clear: true, noCoverage: true}));
 });
 
 gulp.task('watch', function() {
 	gulp.watch([
-		'lib/**/*.php',
+		'library/**/*.php',
 		'tests/**/*.php'
 	], ['phpunit']);
 });
