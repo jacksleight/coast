cf269d50d99064def2dd5f0fb3fbb358220fe4ba
Jack Sleight <jacksleight@gmail.com>
2017-08-31 13:57:13 +0100

    Tweak controller behaviour with group/namespace names

--- library/Controller.php
+++ library/Controller.php
@@ -120,17 +120,18 @@ class Controller implements \Coast\App\Access, \Coast\Router\Routable
             $this->_history[] = $item;
             list($controller, $action, $params, $group) = $item;
 
-            if (!isset($this->_nspaces[$group])) {
-                throw new Controller\Exception("Controller group '{$group}' does not exist");
+            $nspace = str_camel_lower($group);
+            if (!isset($this->_nspaces[$nspace])) {
+                throw new Controller\Exception("Controller group '{$nspace}' does not exist");
             }
-            $class = $this->_nspaces[$group] . '\\' . implode('\\', array_map('Coast\str_camel_upper', explode('/', $controller)));
+            $class = $this->_nspaces[$nspace] . '\\' . implode('\\', array_map('Coast\str_camel_upper', explode('/', $controller)));
             if (!isset($this->_actions[$class])) {
                 if (!class_exists($class)) {
-                    throw new Controller\Failure("Controller '{$group}:{$controller}' does not exist");
+                    throw new Controller\Failure("Controller class '{$class}' does not exist");
                 }
                 $object = new $class($this);
                 if (!$object instanceof \Coast\Controller\Action) {
-                    throw new Controller\Exception("Controller '{$group}:{$controller}' is not an instance of \Coast\Controller\Action");
+                    throw new Controller\Exception("Controller class '{$class}' is not an instance of \Coast\Controller\Action");
                 }
                 $this->_actions[$class] = $object;
             } else {
@@ -138,7 +139,7 @@ class Controller implements \Coast\App\Access, \Coast\Router\Routable
             }
             $method = str_camel_lower($action);
             if (!method_exists($object, $method)) {
-                throw new Controller\Failure("Controller action '{$group}:{$controller}:{$action}' does not exist");
+                throw new Controller\Failure("Controller action '{$class}::{$method}' does not exist");
             }
 
             $result = call_user_func_array([$object, $method], $params);
