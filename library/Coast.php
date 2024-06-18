374d0fb4a2dac2d96c8ba0e47cd3f9d7dcd69598
Jack Sleight <jacksleight@gmail.com>
2024-06-18 13:44:03 +0100

    Update version

--- library/Coast.php
+++ library/Coast.php
@@ -1,12 +1,12 @@
 <?php
-/* 
+/*
  * Copyright 2019 Jack Sleight <http://jacksleight.com/>
- * This source file is subject to the MIT license that is bundled with this package in the file LICENCE. 
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENCE.
  */
 
 namespace Coast;
 
 class Coast
 {
-    const VERSION = '0.8.10';
+    const VERSION = '0.8.11';
 }
